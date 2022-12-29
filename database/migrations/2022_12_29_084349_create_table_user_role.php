7326a97f24a624a45eb29db83d17b4755abcf463
manhalvin <quocmanh.tran@gumiviet.com>
2022-12-29 15:53:07 +0700

    feat: Create table for partition

---
+++ database/migrations/2022_12_29_084349_create_table_user_role.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_role', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')
+                ->nullable();
+            $table->integer('role_id')
+                ->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_role');
+    }
+};
---
+++ database/migrations/2022_12_29_084439_create_table_permission_role.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permission_role', function (Blueprint $table) {
+            $table->id();
+            $table->integer('permission_id')
+                ->nullable();
+            $table->integer('role_id')
+                ->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permission_role');
+    }
+};
